fbd3ea77f406af590fa72e4193b9064df2537a72
liuyingbin <liuyingbin@dianmi365.com>
2017-06-20 10:01:33 +0800

    update celery

--- celery_log_cut_daily.sh
+++ celery_log_cut_daily.sh
@@ -7,8 +7,12 @@ logs=`ps -ef|grep celery|grep -v grep|tr ' ' '\n'|grep "\-\-logfile="|awk -F= '{
 for file in $logs
 do
 	echo $file
-	newfile=`echo $file|sed s/\.log$/-${yesday}\.log/g`
-	echo "NEW file:$newfile"
-	mv $file $newfile
-	[ $? -ne 0 ] && echo "replace $file failed,need check"
+	if [ -s $file ];then
+		newfile=`echo $file|sed s/\.log$/-${yesday}\.log/g`
+		echo "NEW file:$newfile"
+	#	mv $file $newfile
+		cp $file $newfile
+		echo > $file
+		[ $? -ne 0 ] && echo "replace $file failed,need check"
+	fi
 done
